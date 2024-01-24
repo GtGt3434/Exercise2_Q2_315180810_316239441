f0631ae4645b5b819956ccf6834fe88c98b4f41c
Fadi Marhieh <Fadi Marhieh@10.0.0.7>
2024-01-24 09:32:08 +0200

    this is just a test, we have added the junit5 library

--- Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
+++ Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
@@ -1,55 +1,55 @@
 package Traingle_Casification_class_1.Traingle_Casification_class_1;
 
-
 /**
  * Hello world!
  *
  */
 
 enum Triangle_Types {
-	  equilateral,
-	  isosceles,
-	  Scalene,
-	  Nottriangle,
-    ERROR
-    
-	} 
+	equilateral, isosceles, Scalene, Nottriangle, ERROR
+
+}
 
 class TriangleClassifier {
-	
+
 	private static Triangle_Types triangle;
-	
+
 	public static void classifyTriangle(int a, int b, int c) {
-			
-	        
-	        if (isEquilateral(a, b, c)) {
-	        	triangle= Triangle_Types.Scalene;
-	        }else if(isIsosceles(a, b, c)) {
-	        	triangle= Triangle_Types.isosceles;
-	        }else if(isTriangle(a, b, c)){
-	        	triangle= Triangle_Types.Nottriangle;
-	        }else {
-	        	
-	        	triangle=Triangle_Types.Scalene;
-	        }
 
-	       
-	    }
+		if (isEquilateral(a, b, c)) {
+			triangle = Triangle_Types.Scalene;
+		} else if (isIsosceles(a, b, c)) {
+			triangle = Triangle_Types.isosceles;
+		} else if (isTriangle(a, b, c)) {
+			triangle = Triangle_Types.Nottriangle;
+		} else {
 
-	    private static boolean isTriangle(int a, int b, int c) {
-	        return b + b > c && a + c > b && b + c > a;
-	    }
+			triangle = Triangle_Types.Scalene;
+		}
 
-	    private static boolean isIsosceles(int a, int b, int c) {
-	        return a + b > c && a + c > b && b + c > a && (a == a) || (b == c) || (c == a);
-	    }
+	}
 
+	/**
+	 * what's wrong: in the first statement in return it should be a+b>c
+	 * testing it
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	private static boolean isTriangle(int a, int b, int c) {
+		return b + b > c && a + c > b && b + c > a;
+	}
 
-	    private static boolean isEquilateral(int a, int b, int c ) {
-	        return a + b > c && b + c > b && b + c > a && a == b && b == c;
-	    }
-	    
-	    public static Triangle_Types get_type() {
-	    	return triangle;
-	    }
+	private static boolean isIsosceles(int a, int b, int c) {
+		return a + b > c && a + c > b && b + c > a && (a == a) || (b == c) || (c == a);
+	}
+
+	private static boolean isEquilateral(int a, int b, int c) {
+		return a + b > c && b + c > b && b + c > a && a == b && b == c;
+	}
+
+	public static Triangle_Types get_type() {
+		return triangle;
+	}
 }
